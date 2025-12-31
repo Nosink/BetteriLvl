888e849b3cde5a4966eaa02aad2944824d0e85ab
NosinkD <ricardruizc@gmail.com>
2025-12-31 22:48:16 +0100

    betters

--- configuration/commands.lua
+++ configuration/commands.lua
@@ -1,9 +1,20 @@
-local _, ns = ...
+local name, ns = ...
 
 SLASH_BETTERILVL1 = "/betterilvl"
 SLASH_BETTERILVL2 = "/bilvl"
-SlashCmdList.BETTERILVL = function()
+
+local function openSettings()
     if Settings and Settings.OpenToCategory and ns.settingsCategory then
         Settings.OpenToCategory(ns.settingsCategory:GetID())
+    end
+end
+
+SlashCmdList.BETTERILVL = function(msg)
+    msg = (msg or ""):match("^%s*(.-)%s*$"):lower()
+
+    if msg == "" or msg == "config" or msg == "options" or msg == "settings" then
+        return openSettings()
+    else
+        print("|cffffd200" .. name  .. ":|r Unknown Command:", msg)
     end
 end
