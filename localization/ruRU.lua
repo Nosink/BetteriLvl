8c4a28f918de2e4393d0691d3f5911260239ee5a
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-01-14 13:30:23 +0300

    Create ruRU.lua (#2)

---
+++ localization/ruRU.lua
@@ -0,0 +1,24 @@
+local name, ns = ...
+local L = ns.L
+-- Translator ZamestoTV
+if ns.locale == "ruRU" or ns.locale == "ruRU" then
+L["LKEY_OPTIONS_TITLE"] = name .. " " .. "Настройки"
+
+L["LKEY_OPTIONS_PLAYER_FRAME"] = "Фрейм персонажа"
+L["LKEY_OPTIONS_PLAYER_AVERAGE"] = "Показывать средний уровень предметов"
+L["LKEY_OPTIONS_PLAYER_AVERAGE_ALTERNATE_POSITION"] = "Средний уровень предметов под персонажем"
+L["LKEY_OPTIONS_PLAYER_ITEM_LEVEL"] = "Показывать уровень предметов"
+L["LKEY_OPTIONS_PLAYER_BORDER"] = "Показывать рамку"
+
+L["LKEY_OPTIONS_TARGET_FRAME"] = "Фрейм цели"
+L["LKEY_OPTIONS_TARGET_AVERAGE"] = "Показывать средний уровень предметов"
+L["LKEY_OPTIONS_TARGET_ITEM_LEVEL"] = "Показывать уровень предметов"
+L["LKEY_OPTIONS_TARGET_BORDER"] = "Показывать рамку"
+
+L["LKEY_OPTIONS_TOOLTIP"] = "Подсказки"
+L["LKEY_OPTIONS_TOOLTIP_ITEM_LEVEL"] = "Показывать уровень предметов"
+L["LKEY_OPTIONS_TOOLTIP_ITEM_ID"] = "Показывать ID предмета"
+
+L["LKEY_TOOLTIP_ITEM_LEVEL"] = "Уровень предмета"
+L["LKEY_TOOLTIP_ITEM_ID"] = "ID предмета"
+end
