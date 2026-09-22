0f3fcf6b2bd212ba6006dc83bb0c3d8fca80c86c
NosinkD <ricardruizc@gmail.com>
2026-09-22 12:11:59 +0200

    added and fixed loacles

--- addon/locales/itIT.lua
+++ addon/locales/itIT.lua
@@ -6,18 +6,18 @@ if ns.locale == "itIT" then
 
     L["LKEY_OPTIONS_PLAYER_FRAME"] = "Riquadro del personaggio"
     L["LKEY_OPTIONS_PLAYER_ITEM_LEVEL"] = "Mostra il livello dell'oggetto"
-    L["LKEY_OPTIONS_PLAYER_BORDER"] = "Mostra il bordo della qualita"
+    L["LKEY_OPTIONS_PLAYER_BORDER"] = "Mostra il bordo della qualità"
 
-    L["LKEY_OPTIONS_DURABILITY"] = "Durabilita"
-    L["LKEY_OPTIONS_DURABILITY_ENABLE"] = "Attiva la visualizzazione della durabilita"
-    L["LKEY_OPTIONS_DURABILITY_TYPE"] = "Tipo di visualizzazione della durabilita"
+    L["LKEY_OPTIONS_DURABILITY"] = "Durabilità"
+    L["LKEY_OPTIONS_DURABILITY_ENABLE"] = "Attiva la visualizzazione della durabilità"
+    L["LKEY_OPTIONS_DURABILITY_TYPE"] = "Tipo di visualizzazione della durabilità"
 
     L["LKEY_OPTIONS_DURABILITY_TYPE_BAR"] = "Barra"
     L["LKEY_OPTIONS_DURABILITY_TYPE_TEXT"] = "Testo"
 
-    L["LKEY_OPTIONS_DURABILITY_COLOR"] = "Mostra la durabilita a colori"
+    L["LKEY_OPTIONS_DURABILITY_COLOR"] = "Mostra la durabilità a colori"
 
     L["LKEY_OPTIONS_TARGET_FRAME"] = "Riquadro del bersaglio"
     L["LKEY_OPTIONS_TARGET_ITEM_LEVEL"] = "Mostra il livello dell'oggetto"
-    L["LKEY_OPTIONS_TARGET_BORDER"] = "Mostra il bordo della qualita"
+    L["LKEY_OPTIONS_TARGET_BORDER"] = "Mostra il bordo della qualità"
 end
--- addon/locales/ptBR.lua
+++ addon/locales/ptBR.lua
@@ -5,12 +5,12 @@ if ns.locale == "ptBR" then
     L["LKEY_OPTIONS_TITLE"] = "Better iLvl"
 
     L["LKEY_OPTIONS_PLAYER_FRAME"] = "Quadro do personagem"
-    L["LKEY_OPTIONS_PLAYER_ITEM_LEVEL"] = "Exibir nivel do item"
+    L["LKEY_OPTIONS_PLAYER_ITEM_LEVEL"] = "Exibir nível do item"
     L["LKEY_OPTIONS_PLAYER_BORDER"] = "Exibir borda de qualidade"
 
     L["LKEY_OPTIONS_DURABILITY"] = "Durabilidade"
-    L["LKEY_OPTIONS_DURABILITY_ENABLE"] = "Ativar exibicao de durabilidade"
-    L["LKEY_OPTIONS_DURABILITY_TYPE"] = "Tipo de exibicao de durabilidade"
+    L["LKEY_OPTIONS_DURABILITY_ENABLE"] = "Ativar exibição de durabilidade"
+    L["LKEY_OPTIONS_DURABILITY_TYPE"] = "Tipo de exibição de durabilidade"
 
     L["LKEY_OPTIONS_DURABILITY_TYPE_BAR"] = "Barra"
     L["LKEY_OPTIONS_DURABILITY_TYPE_TEXT"] = "Texto"
@@ -18,6 +18,6 @@ if ns.locale == "ptBR" then
     L["LKEY_OPTIONS_DURABILITY_COLOR"] = "Exibir durabilidade colorida"
 
     L["LKEY_OPTIONS_TARGET_FRAME"] = "Quadro do alvo"
-    L["LKEY_OPTIONS_TARGET_ITEM_LEVEL"] = "Exibir nivel do item"
+    L["LKEY_OPTIONS_TARGET_ITEM_LEVEL"] = "Exibir nível do item"
     L["LKEY_OPTIONS_TARGET_BORDER"] = "Exibir borda de qualidade"
 end
