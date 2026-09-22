811646c8e1f08cd629c69f9fa3fe77b32658b15a
NosinkD <ricardruizc@gmail.com>
2026-09-22 15:57:38 +0200

    ended commands.lua

--- addon/settings/commands.lua
+++ addon/settings/commands.lua
@@ -1,22 +1,26 @@
 local name, ns = ...
 local L = ns.L
 
-SLASH_BETTERILVL1 = "/betterilvl"
-SLASH_BETTERILVL2 = "/bilvl"
+SLASH_BETTERILVL1 = "/bilvl"
+SLASH_BETTERILVL2 = "/betterilvl"
 
-local function openSettings()
+local function openSettingsPanel()
     if Settings and Settings.OpenToCategory and ns.settingsCategory then
         Settings.OpenToCategory(ns.settingsCategory:GetID())
     end
+end
+
+local function displayCommandHelp()
+    print(string.format("|c%s%s:|r %s |cff00ff00/betterilvl|r %s |cff00ff00/bilvl|r %s.",
+        "ffffd200", name, L["COMMAND_USE"], L["COMMAND_OR"], L["COMMAND_DESCRIPTION"]))
 end
 
 SlashCmdList.BETTERILVL = function(message)
     message = (message or ""):match("^%s*(.-)%s*$"):lower()
 
     if (message == "" or message == "config" or message == "options" or message == "settings") then
-        return openSettings()
+        openSettingsPanel()
     else
-        print(string.format("|c%s%s:|r %s |cff00ff00/betterilvl|r %s |cff00ff00/bilvl|r %s.",
-            "ffffd200", name, L["COMMAND_USE"], L["COMMAND_OR"], L["COMMAND_DESCRIPTION"]))
+        displayCommandHelp()
     end
 end
